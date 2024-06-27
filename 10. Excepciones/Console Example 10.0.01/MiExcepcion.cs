d82a3c4dd392504383d597db537dc84507af9723
Juan Beltrame <juan_beltrame@hotmail.com>
2024-06-27 14:02:31 -0300

    Update

---
+++ 10. Excepciones/Console Example 10.0.01/MiExcepcion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Example_10._0._01
+{
+    internal class MiExcepcion : Exception
+    {
+            
+    }
+}
--- 10. Excepciones/Console Example 10.0.01/Program.cs
+++ 10. Excepciones/Console Example 10.0.01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//--------------------------------------------------------
+int[] array = { 1, 2, 3 };
+array[4] = 4; // IndexOutOfRangeException
+
+//--------------------------------------------------------
+int num1 = 4;
+int num2 = 0;
+int var = num1 / num2; // DivideByZeroException
