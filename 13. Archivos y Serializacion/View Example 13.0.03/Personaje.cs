80d57b100c0b91840cb0753e6319adaf952a89b9
Juan Beltrame <juan_beltrame@hotmail.com>
2024-09-28 09:31:22 -0300

    Serializacion

---
+++ 13. Archivos y Serializacion/View Example 13.0.03/Personaje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View_Example_13._0._03
+{
+    public class Personaje
+    {
+        private int puntosdeVida;
+        private string nombre;
+
+        public int PuntosdeVida { get => puntosdeVida; set => puntosdeVida = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public Personaje()
+        {
+        }
+
+        public Personaje(int puntosdeVida, string nombre)
+        {
+            this.puntosdeVida = puntosdeVida;
+            this.nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {nombre}, Puntos de vida: {puntosdeVida}";
+        }
+    }
+}
---
+++ 13. Archivos y Serializacion/View Example 13.0.03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using View_Example_13._0._03;
+
+static void SerializarAXML<T>(string ruta, T objeto)
+{
+    using (StreamWriter sw = new(ruta))
+    {
+        XmlSerializer xmlSerializer = new(typeof(T));
+        xmlSerializer.Serialize(sw, objeto);
+    }
+}
+
+
+static T DeserializarAXML<T>(string ruta) where T : class
+{
+    using (StreamReader sr = new(ruta))
+    {
+        XmlSerializer xmlSerializer = new(typeof(T));
+        T objeto = (xmlSerializer.Deserialize(sr) as T)!;
+        return objeto;
+    }
+}
+
+
+string rutaEscritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+string rutaArchivo = Path.Combine(rutaEscritorio, "personaje.xmlS");
+
+// Personaje personaje = new (100, "CarpinchoAsesino");
+// SerializarAXML(rutaArchivo, personaje);
+Personaje personaje = DeserializarAXML<Personaje>(rutaArchivo);
+Console.WriteLine(personaje.ToString());
+
