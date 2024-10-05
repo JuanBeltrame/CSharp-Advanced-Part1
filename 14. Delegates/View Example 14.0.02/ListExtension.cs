d2b723be204d41f8fed6790a74f41f7a37da621a
Juan Beltrame <juan_beltrame@hotmail.com>
2024-10-05 07:47:32 -0300

    Update Delegates

---
+++ 14. Delegates/View Example 14.0.02/ListExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ListExtension
+{
+    public static List<T> Filtrar<T>(this List<T> lista, DelegadoCriterio<T> criterioDeFiltrado)
+    {
+        List<T> listaFiltrada = new List<T>();
+
+        foreach (T item in lista)
+        {
+            if (criterioDeFiltrado(item))
+                listaFiltrada.Add(item);
+        }
+
+        return listaFiltrada;
+    }
+
+    // Usando Delegados Predefinidos // En este caso Predicate
+    public static List<T> Filtrar<T>(this List<T> lista, Predicate<T> criterioDeFiltrado)
+    {
+        List<T> listaFiltrada = new List<T>();
+
+        foreach (T item in lista)
+        {
+            if (criterioDeFiltrado(item))
+                listaFiltrada.Add(item);
+        }
+
+        return listaFiltrada;
+    }
+
+    // Usando Delegados Predefinidos // En este caso Func
+    public static List<T> Filtrar<T>(this List<T> lista, Func<T, bool> criterioDeFiltrado)
+    {
+        List<T> listaFiltrada = new List<T>();
+
+        foreach (T item in lista)
+        {
+            if (criterioDeFiltrado(item))
+                listaFiltrada.Add(item);
+        }
+
+        return listaFiltrada;
+    }
+}
+
---
+++ 14. Delegates/View Example 14.0.02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+
+List<int> list;
+
+void MostrarNumeros()
+{
+    string numeros = "";
+
+    foreach (int numero in list)
+        numeros += $"{numero} | ";
+
+    Console.WriteLine($"Numeros antes de filtrar: {numeros}");
+}
+
+static bool EsPar(int numero) => numero % 2 == 0;
+
+static bool EsNegativo(int numero) => numero < 0;
+
+
+Console.BackgroundColor = ConsoleColor.Black;
+
+list = new List<int>()
+            {
+                 3, 45, 6, -9, 70, -134, 247, 17
+            };
+MostrarNumeros();
+
+list = list.Filtrar(EsPar);
+MostrarNumeros();
+list = list.Filtrar(EsNegativo);
+
+MostrarNumeros();
+
+
