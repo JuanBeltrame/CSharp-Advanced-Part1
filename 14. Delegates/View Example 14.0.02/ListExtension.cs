132b91bac339a467d90e4e8ac10d02ac1dd7e99f
Juan Beltrame <juan_beltrame@hotmail.com>
2024-10-06 09:39:08 -0300

    Terminar con Delegados

--- 14. Delegates/View Example 14.0.02/ListExtension.cs
+++ 14. Delegates/View Example 14.0.02/ListExtension.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 public static class ListExtension
 {
-    public static List<T> Filtrar<T>(this List<T> lista, DelegadoCriterio<T> criterioDeFiltrado)
-    {
-        List<T> listaFiltrada = new List<T>();
+    //public static List<T> Filtrar<T>(this List<T> lista, DelegadoCriterio<T> criterioDeFiltrado)
+    //{
+    //    List<T> listaFiltrada = new List<T>();
 
-        foreach (T item in lista)
-        {
-            if (criterioDeFiltrado(item))
-                listaFiltrada.Add(item);
-        }
+    //    foreach (T item in lista)
+    //    {
+    //        if (criterioDeFiltrado(item))
+    //            listaFiltrada.Add(item);
+    //    }
 
-        return listaFiltrada;
-    }
+    //    return listaFiltrada;
+    //}
 
-    // Usando Delegados Predefinidos // En este caso Predicate
+    //Usando Delegados Predefinidos // En este caso Predicate
     public static List<T> Filtrar<T>(this List<T> lista, Predicate<T> criterioDeFiltrado)
     {
         List<T> listaFiltrada = new List<T>();
@@ -34,17 +34,17 @@ public static class ListExtension
     }
 
     // Usando Delegados Predefinidos // En este caso Func
-    public static List<T> Filtrar<T>(this List<T> lista, Func<T, bool> criterioDeFiltrado)
-    {
-        List<T> listaFiltrada = new List<T>();
+    //public static List<T> Filtrar<T>(this List<T> lista, Func<T, bool> criterioDeFiltrado)
+    //{
+    //    List<T> listaFiltrada = new List<T>();
 
-        foreach (T item in lista)
-        {
-            if (criterioDeFiltrado(item))
-                listaFiltrada.Add(item);
-        }
+    //    foreach (T item in lista)
+    //    {
+    //        if (criterioDeFiltrado(item))
+    //            listaFiltrada.Add(item);
+    //    }
 
-        return listaFiltrada;
-    }
+    //    return listaFiltrada;
+    //}
 }
 
