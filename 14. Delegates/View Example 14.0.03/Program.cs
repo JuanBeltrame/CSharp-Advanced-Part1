933c0dae8a694d8d64d0b564b26240700427d04f
Juan Beltrame <juan_beltrame@hotmail.com>
2024-10-07 07:16:43 -0300

    Update Delegates

---
+++ 14. Delegates/View Example 14.0.03/Program.cs
@@ -0,0 +1,45 @@
+﻿using View_Example_14._0._03;
+
+public delegate void DelegadoSinParmetros();
+public delegate void DelegadoConUnInt(int milisegundos);
+public delegate int DelegadoComparar(int numero1, int numero2);
+public delegate void DelegadoMostrar(int numero1, int numero2);
+public delegate bool DelegadoCriterio(int numero);
+
+public class Program
+{
+    static void Saludar() => Console.WriteLine("Hola Mundo");
+
+    static void SaludarConMilisegundos(int milisegundos) => Console.WriteLine($"Hola Mundo, pasaron {milisegundos} milisegundos");
+
+    static void SaludarConMilisegundos(int milisegundosMayor, int milisegundosMenor) => Console.WriteLine($"Hola Mundo, pasaron {milisegundosMayor} milisegundos... (mayor a {milisegundosMenor})");
+
+    static int Comparar(int numeroUno, int numeroDos) => numeroUno - numeroDos;
+
+    static bool EsPar(int numero) => numero % 2 != 0;
+
+    static void MostrarOk(int milisegundos) => Console.WriteLine($"{milisegundos} paso el criterio y se espero...");
+
+    static void MostrarNoOk(int milisegundos) => Console.WriteLine($"{milisegundos} No paso el criterio y No se espero...");
+
+    private static void Main(string[] args)
+    {
+        DelegadoSinParmetros delegadoSaludar = Saludar;
+        DelegadoConUnInt delegadoSaludarConMilisegundos = SaludarConMilisegundos;
+        DelegadoComparar delegadoComparar = Comparar;
+        DelegadoMostrar delegadoMostrar = SaludarConMilisegundos;
+        DelegadoCriterio delegadoCriterio = EsPar;
+        DelegadoConUnInt delegadoOk = MostrarOk;
+        DelegadoConUnInt delegadoNoOk = MostrarNoOk;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Temporizador.Esperar(new Random().Next(3000, 5000), delegadoCriterio, delegadoOk, delegadoNoOk);
+        }
+
+
+        Temporizador.Esperar(new Random().Next(3000, 5000), delegadoSaludar);
+        Temporizador.Esperar(new Random().Next(3000, 5000), delegadoSaludarConMilisegundos);
+        Temporizador.EsperarElTIempo(new Random().Next(3000, 5000), new Random().Next(3000, 5000), delegadoComparar, delegadoMostrar);
+    }
+}
---
+++ 14. Delegates/View Example 14.0.03/Temporizador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View_Example_14._0._03
+{
+    public class Temporizador
+    {
+        public static void Esperar(int milisegundos, DelegadoSinParmetros delegadoSinParmetros)
+        {
+            Thread.Sleep(milisegundos);
+            delegadoSinParmetros();
+        }
+
+        public static void Esperar(int milisegundos, DelegadoConUnInt delegadoConUnInt)
+        {
+            Thread.Sleep(milisegundos);
+            delegadoConUnInt(milisegundos);
+        }
+
+        public static void Esperar(int milisegundos, DelegadoCriterio delegadoCriterio, DelegadoConUnInt delegadoResultadoOk, DelegadoConUnInt delegadoResultadoNoOk)
+        {
+            if (delegadoCriterio(milisegundos))
+            {
+                Thread.Sleep(milisegundos);
+                delegadoResultadoOk(milisegundos);
+            }
+            else
+            {
+                delegadoResultadoNoOk(milisegundos);
+            }
+        }
+
+        public static void EsperarElTIempo(int milisegundosUno, int milisegundosDos, DelegadoComparar delegadoComparar, DelegadoMostrar delegadoMostrar)
+        {
+            int milisegundosMayor;
+            int milisegundosMenor;
+
+            if (delegadoComparar(milisegundosUno, milisegundosDos) >= 0)
+            {
+                milisegundosMayor = milisegundosUno;
+                milisegundosMenor = milisegundosDos;
+            }
+            else
+            {
+                milisegundosMayor = milisegundosDos;
+                milisegundosMenor = milisegundosUno;
+            }
+
+            Thread.Sleep(milisegundosMayor);
+            delegadoMostrar(milisegundosMayor, milisegundosMenor);
+        }
+    }
+}
