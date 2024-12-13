9cc9ef0fc65908cccb3035b154258d9272e3c353
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-13 16:47:20 -0300

    Update SQL

--- 14. Conexion a Bases de Datos/Library Example 14.0.01/PersonaAccesoDatos.cs
+++ 14. Conexion a Bases de Datos/Library Example 14.0.01/PersonaAccesoDatos.cs
@@ -16,7 +16,7 @@ namespace Library_Example_14._0._01
 
         static PersonaAccesoDatos()
         {
-            connectionString = "Server=.;Database=UTN_DB;Trusted_Connection=True";
+            connectionString = "Server=.;Database=UTN_DB;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
             connection = new SqlConnection(connectionString);
             command = new SqlCommand();
             command.Connection = connection;
