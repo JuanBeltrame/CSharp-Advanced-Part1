a652016eb1aa57012bd8d1d78b77a6c1bc1ea5ce
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-13 16:24:58 -0300

    Update SQL

--- 14. Conexion a Bases de Datos/Library Example 14.0.01/PersonaAccesoDatos.cs
+++ 14. Conexion a Bases de Datos/Library Example 14.0.01/PersonaAccesoDatos.cs
@@ -17,10 +17,36 @@ namespace Library_Example_14._0._01
         static PersonaAccesoDatos()
         {
             connectionString = "Server=.;Database=UTN_DB;Trusted_Connection=True";
-            command = new SqlCommand();
             connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
             command.Connection = connection;
             command.CommandType = System.Data.CommandType.Text;
+        }
+
+        public static List<Persona> Leer()
+        {
+            List<Persona> personas = new();
+
+            try
+            {
+                connection.Open();
+                command!.CommandText = "SELECT * FROM Persona";
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    personas.Add(new Persona(dataReader["nombre"].ToString()!, dataReader["apellido"].ToString()!));
+                }
+                return personas;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
