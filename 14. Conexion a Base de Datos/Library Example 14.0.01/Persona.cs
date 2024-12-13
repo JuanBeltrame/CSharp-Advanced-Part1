b132614bcbabe2d5e5f1fe8ac2bd62486af295bd
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-13 06:02:40 -0300

    Sql Connection

---
+++ 14. Conexion a Base de Datos/Library Example 14.0.01/Persona.cs
@@ -0,0 +1,17 @@
+﻿namespace Library_Example_14._0._01
+{
+    public class Persona
+    {
+        string? nombre;
+        string? apellido; 
+
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public override string ToString() => $"Nombre: {nombre}, Apellido: {apellido}";
+        
+    }
+}
---
+++ 14. Conexion a Base de Datos/Library Example 14.0.01/PersonaAccesoDatos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Example_14._0._01
+{
+    public static class PersonaAccesoDatos
+    {
+        static string? connectionString;
+        static SqlCommand? command;
+        static SqlConnection connection;
+
+        static PersonaAccesoDatos()
+        {
+            connectionString = "Server=.;Database=UTN_DB;Trusted_Connection=True";
+            connection = new SqlConnection(connectionString);
+        }
+    }
+}
